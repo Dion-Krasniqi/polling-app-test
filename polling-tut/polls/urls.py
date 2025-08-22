79fd189a6317c8f0f95b4616aa2d1b5921cd9f37
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-08-22 15:59:53 +0200

    Changed the detail and results view to implemnt generic views

--- polling-tut/polls/urls.py
+++ polling-tut/polls/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"), # something like /polls/
-    path("<int:question_id>/", views.detail, name="detail") ,# /polls/5/
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("", views.IndexView.as_view(), name="index"), # something like /polls/
+    path("<int:pk>/", views.DetailView.as_view(), name="detail") ,# /polls/5/
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
